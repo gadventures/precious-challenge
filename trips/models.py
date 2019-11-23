aa75e7071ffd5b82c8ac1328edd1891b60300243
Salinder Sidhu <salinder.sid@gmail.com>
2019-11-23 00:32:07 -0500

    Integrated Service model into Trip model

--- trips/models.py
+++ trips/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+class Service(models.Model):
+    name = models.CharField(
+        max_length=300,
+        unique=True,
+        help_text="The name of the service"
+    )
+    location = models.CharField(
+        max_length=300,
+        help_text="The location of the service"
+    )
+    # TODO: Perhapse an Enum would be a better option here
+    type = models.CharField(
+        max_length=300,
+        help_text="The type of service"
+    )
+    cost = models.IntegerField(help_text="Total cost of the service")
+
 class Trip(models.Model):
     title = models.CharField(
         max_length=300,
@@ -16,19 +33,4 @@ class Trip(models.Model):
     )
     cost = models.IntegerField(help_text="Total cost of the trip")
     duration_days = models.IntegerField(help_text="How many days this trip runs")
-
-class Service(models.Model):
-    name = models.CharField(
-        max_length=300,
-        unique=True,
-        help_text="The name of the service"
-    )
-    location = models.CharField(
-        max_length=300,
-        help_text="The location of the service"
-    )
-    type = models.CharField(
-        max_length=300,
-        help_text="The type of service"
-    )
-    cost = models.IntegerField(help_text="Cost of the service")
+    services = models.ManyToManyField(Service)
