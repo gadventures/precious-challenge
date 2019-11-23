e0d95936df58cc774ea8891408bcc2af35f88b76
Salinder Sidhu <salinder.sid@gmail.com>
2019-11-23 00:26:29 -0500

    Added Service model

--- trips/models.py
+++ trips/models.py
@@ -16,3 +16,19 @@ class Trip(models.Model):
     )
     cost = models.IntegerField(help_text="Total cost of the trip")
     duration_days = models.IntegerField(help_text="How many days this trip runs")
+
+class Service(models.Model):
+    name = models.CharField(
+        max_length=300,
+        unique=True,
+        help_text="The name of the service"
+    )
+    location = models.CharField(
+        max_length=300,
+        help_text="The location of the service"
+    )
+    type = models.CharField(
+        max_length=300,
+        help_text="The type of service"
+    )
+    cost = models.IntegerField(help_text="Cost of the service")
