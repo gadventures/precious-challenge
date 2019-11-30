fa1e487431b811f5377cc0f274bbc016b260b36f
Brandon Marino <brandonmarino@Brandons-MacBook-Pro.local>
2019-11-30 18:50:18 -0500

    Adding a query for the categories table. I seperated the categories query out because it will be used for service dropdowns as well as displaying the category type of existing services.

--- static/js/components/app.js
+++ static/js/components/app.js
@@ -9,7 +9,8 @@ export default class App extends Component {
 
         this.state = {
             // create an empty array that will hold the trips
-            trips: []
+            trips: [],
+            categoryMap: new Map()
         }
 
         this.getTrips = this.getTrips.bind(this);
@@ -27,6 +28,7 @@ export default class App extends Component {
                     <div key={i} className=".col-md-4">
                         <Trip
                             trip={this.state.trips[i]}
+                            updateTrips={this.getTrips}
                         />
                     </div>
                 ))}
@@ -34,13 +36,40 @@ export default class App extends Component {
         );
     }
 
-    // getTrips makes a call to /api/ which returns the trip data in JSON format
+    /** 
+     * getTrips makes a call to /api/ which returns the trip data in JSON format
+     */
     getTrips() {
-        $.getJSON({
-            url: "/api/trips",
-        }).then((trips) => this.setState({ trips: trips })).catch(
+        const getTripDataPromises = [];
+        // first query for all trips
+        getTripDataPromises.push($.getJSON({ url: "/api/trips" }));
+        // I'm getting the categories seperately
+        getTripDataPromises.push($.getJSON({ url: "/api/categories" }));
+
+        Promise.all(getTripDataPromises).then((queryResponses) => {
+            // update the trips
+            this.state.trips = queryResponses[0];
+
+            // create a map for the categories instead of using an array 
+            // I don't want to loop to search for a category by id
+            const categoryMap = new Map();
+
+            // loop through the categories and assign each record to a map with id as the key
+            queryResponses[1].map((currentCategory) => {
+                // store the reference to the full category. 
+                // We may have more properties than just 'display_name' in the future.
+                // this is really just future proofing, I could have pused the string instead.
+                categoryMap.set(currentCategory.id, currentCategory);
+            });
+
+            // keep a reference to the categorymap in the state
+            this.state.categoryMap = categoryMap;
+
+            // set the state and update render the view
+            this.setState(this.state);
+        }).catch(
             (error) => {
-                console.log("Oops - ", error)
-            })
+                console.error("Error Requesting Trip Data - ", error)
+            });
     }
 }
