d037c67f7c80ddc9b1b882cd3740a1ea687531a2
Brandon Marino <brandonmarino@Brandons-MacBook-Pro.local>
2019-11-30 19:04:24 -0500

    Adding material-ui to the project. I'm going to use this set of components to improve the design of the page and I'll use it as a base for adding the ability to add new services for trips as well.

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app'
 
 ReactDOM.render(
-    <App />,
+    <div>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <App />,
+    </div>,
     document.getElementById('container')
 )
