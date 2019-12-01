8e4b8e846be43eb31b406a361ee8ded88e7d09d1
Brandon Marino <brandonmarino@Brandons-MacBook-Pro.local>
2019-11-30 23:13:43 -0500

    Pushing my changes to register Categories and Services with the admin portal

--- trips/admin.py
+++ trips/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from trips.models import Trip
+from trips.models import Trip, Category, Service
 
 
 class TripsAdmin(admin.ModelAdmin):
@@ -8,4 +8,18 @@ class TripsAdmin(admin.ModelAdmin):
     # allow for trips to be searched by the title, duration in days and cost
     search_fields = ["title", "duration_days", "cost"]
 
+class CategoryAdmin(admin.ModelAdmin):
+    # display the trips list by their display_name
+    list_display = ("display_name", )
+    # allow for trips to be searched by their display_name
+    search_fields = ["display_name"]
+
+class ServiceAdmin(admin.ModelAdmin):
+    # display the trips list by their name
+    list_display = ("name", )
+    # allow for trips to be searched by their name, location and cost
+    search_fields = ["name", "location", "cost"]
+
 admin.site.register(Trip,TripsAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Service,ServiceAdmin)
