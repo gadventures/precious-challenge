bad05be21e12f9210f7c94ac0b85ff1e618646b4
yankov.martin <lutheriscomming@gmail.com>
2019-12-02 19:25:38 +0200

    Added basic model implementations

--- trips/models.py
+++ trips/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 
+
+class Service(models.Model):
+    """
+    Store all available services
+    """
+    types = (
+        (1, "Hotel"),
+        (2, "Accommodation"),
+        (3, "Transportation"),
+    )
+
+    name = models.CharField(
+        max_length=300,
+        unique=True,
+        help_text="The name of the service",
+    )
+
+    location = models.CharField(
+        max_length=300,
+        help_text="The location of the service",
+    )
+
+    type = models.IntegerField(
+        choices=types,
+        help_text="The type of service (Hotel, Accomodation or Transportation)",
+    )
+
+    cost = models.IntegerField(help_text="The cost of the service")
+
+
 class Trip(models.Model):
     title = models.CharField(
         max_length=300,
@@ -15,4 +45,8 @@ class Trip(models.Model):
         help_text="Initial location of the trip",
     )
     cost = models.IntegerField(help_text="Total cost of the trip")
-    duration_days = models.IntegerField(help_text="How many days this trip runs")
+    duration_days = models.IntegerField(
+        help_text="How many days this trip runs")
+
+    # Associated services
+    services = models.ManyToManyField(Service)
