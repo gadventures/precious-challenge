9c2553ae0e2f7be9796c84ac6428ddcd3347c093
Yonkov <yonkov.atanas@gmail.com>
2019-12-04 10:52:12 +0200

    add view in django for posting transportation services

--- trips/views.py
+++ trips/views.py
@@ -5,12 +5,12 @@ from rest_framework import generics
 from trips.models import Trip
 from trips.models import Hotel
 # from trips.models import Accomodation
-# from trips.models import Transportation
+from trips.models import Transportation
 
 from trips.serializers import TripSerializer
 from trips.serializers import HotelSerializer
 # from trips.serializers import AccomodationSerializer
-# from trips.serializers import TransportationSerializer
+from trips.serializers import TransportationSerializer
 from django.views.generic import DetailView
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -27,14 +27,18 @@ class TripList(generics.ListAPIView):
         queryset = Trip.objects.all()
         return queryset
     
-
 # Details view (view trips detail page)
 class TripDetailView(DetailView):
     model=Trip
     template_name = 'index.html'
-
+# Create views for the Post requests
+# In this way we allow post requests on the frontend
 class PostHotel(generics.ListCreateAPIView):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
+
+class PostTransportation(generics.ListCreateAPIView):
+    queryset = Transportation.objects.all()
+    serializer_class = TransportationSerializer
 
 
