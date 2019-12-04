fd63fae87bea09926f93c500618c710c223f8b37
Yonkov <yonkov.atanas@gmail.com>
2019-12-04 11:13:36 +0200

    add accomodation ednpoint url

--- trips/views.py
+++ trips/views.py
@@ -4,12 +4,12 @@ from rest_framework import generics
 
 from trips.models import Trip
 from trips.models import Hotel
-# from trips.models import Accomodation
+from trips.models import Accomodation
 from trips.models import Transportation
 
 from trips.serializers import TripSerializer
 from trips.serializers import HotelSerializer
-# from trips.serializers import AccomodationSerializer
+from trips.serializers import AccomodationSerializer
 from trips.serializers import TransportationSerializer
 from django.views.generic import DetailView
 from rest_framework.views import APIView
@@ -17,6 +17,9 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
 
+####################################
+# Create views for the GET requests
+####################################
 class TripList(generics.ListAPIView):
     """
     A list of all trips, read-only
@@ -31,8 +34,11 @@ class TripList(generics.ListAPIView):
 class TripDetailView(DetailView):
     model=Trip
     template_name = 'index.html'
+
+####################################
 # Create views for the Post requests
-# In this way we allow post requests on the frontend
+####################################
+
 class PostHotel(generics.ListCreateAPIView):
     """
     API endpoint that allows hotels to be added to a corresponding trip on the frontend.
@@ -45,6 +51,13 @@ class PostTransportation(generics.ListCreateAPIView):
     API endpoint that allows transportations to be added to a corresponding trip on the frontend.
     """
     queryset = Transportation.objects.all()
+    serializer_class = TransportationSerializer
+
+class PostAccomodation(generics.ListCreateAPIView):
+    """
+    API endpoint that allows transportations to be added to a corresponding trip on the frontend.
+    """
+    queryset = Accomodation.objects.all()
     serializer_class = TransportationSerializer
 
 
